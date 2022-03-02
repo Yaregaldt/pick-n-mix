2cdedecf1d45c6855ef2edd2e07fe7fb5b159c3a
Bilal Raza <smbraza97@gmail.com>
2022-03-03 00:03:07 +0500

    added list item routes

---
+++ controllers/api/listRoutes.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const { ListItem } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newListItem = await ListItem.create({
+      quantity: req.body.quantity,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newListItem);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const listItemData = await Post.update(
+      {
+        quantity: req.body.quantity,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!listItemData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(listItemData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const listItemData = await ListItem.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!listItemData) {
+      res.status(404).json({ message: "No list item found with this id!" });
+      return;
+    }
+
+    res.status(200).json(listItemData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
